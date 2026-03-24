f51fa5c2a86e7c1484bc51a55951b26b17b6f953
anzechen <anzechen@bu.edu>
2026-03-23 22:27:18 -0400

    original answer for Q4.2

--- src/Q4.2.rs
+++ src/Q4.2.rs
@@ -4,14 +4,14 @@ enum DivisionResult {
 }
 
 fn divide_with_remainder(x:u32 , y:u32) -> DivisionResult {
-    if y == 0 {return DivisionResult::DivisionByZero;}
+    if y == 0 {return DivisionByZero;}
     else {let quotient = x/y;
         let remainder = x%y;
-        return DivisionResult::OK(quotient, remainder);}
+        return OK(quotient, remainder);}
 }
 
 fn main(){let result = divide_with_remainder(10,3);
-match result {DivisionResult::OK(q,r) => println!("quotient = {}, remainder = {}", q, r),
+match {result::OK => pritln!("quotient = {}, remainder = {}", result.0, result.1),
     _=>println!("cannot divide by zero"),
-    }
+}
 }
